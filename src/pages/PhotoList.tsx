3fad81055d4d059bb0223db5a934953b8a8bf822
tantran <tantran.300803@gmail.com>
2024-10-14 03:09:53 +0700

    Refactor PhotoList component to use Grid instead of Grid2

--- src/pages/PhotoList.tsx
+++ src/pages/PhotoList.tsx
@@ -1,4 +1,4 @@
-import { Grid2, CircularProgress, Box, Typography } from '@mui/material';
+import { Grid, CircularProgress, Box, Typography } from '@mui/material';
 import PhotoCard from '../components/PhotoCard';
 import { usePhotos } from '../hooks/usePhotos';
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -25,17 +25,17 @@ const PhotoList = () => {
                 hasMore={!!hasNextPage}
                 loader={<CircularProgress />}
             >
-                <Grid2 container spacing={3} justifyContent="center">
+                <Grid container spacing={3} justifyContent="center">
                     {data?.pages.flat().map((photo) => (
-                        <Grid2 item key={photo.id}>
+                        <Grid item key={photo.id}>
                             <PhotoCard
                                 id={photo.id}
                                 thumbnailUrl={photo.urls.thumb}
                                 author={photo.user.name}
                             />
-                        </Grid2>
+                        </Grid>
                     ))}
-                </Grid2>
+                </Grid>
 
                 {isFetchingNextPage && (
                     <Box display="flex" justifyContent="center" mt={2}>
